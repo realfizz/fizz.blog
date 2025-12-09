9e1cd4b78b8f88e1f85cb136bf654ff58e3f57dd
Young Fizz <fizzler@black.com>
2025-12-09 10:37:33 +0000

    no scroll hopefully

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${dotoFont.variable} ${jetbrainsMonoFont.variable} ${instrumentSansFont.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${dotoFont.variable} ${jetbrainsMonoFont.variable} ${instrumentSansFont.variable} antialiased no-scrollbar`}
       >
         <ThemeProvider
           attribute="class"
